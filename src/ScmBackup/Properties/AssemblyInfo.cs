d088d2185ec95095dcd262478d6ed6a25beb46f2
Christian Specht <christi@nspecht.de>
2016-01-05 20:36:00 +0100

    assembly info

--- src/ScmBackup/Properties/AssemblyInfo.cs
+++ src/ScmBackup/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScmBackup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SCM Backup")]
+[assembly: AssemblyDescription("Backup repositories from cloud hosters to your local machine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScmBackup")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("christianspecht.de")]
+[assembly: AssemblyProduct("SCM Backup")]
+[assembly: AssemblyCopyright("Copyright © Christian Specht 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
