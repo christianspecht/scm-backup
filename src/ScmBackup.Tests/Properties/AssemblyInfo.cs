b7e4168da1b2c5bb65b82f2f293b514bfd14d86d
Christian Specht <christi@nspecht.de>
2016-01-10 15:10:31 +0100

    assembly info

--- src/ScmBackup.Tests/Properties/AssemblyInfo.cs
+++ src/ScmBackup.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScmBackup.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Test library for SCM Backup")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScmBackup.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("christianspecht.de")]
+[assembly: AssemblyProduct("SCM Backup")]
+[assembly: AssemblyCopyright("Copyright © Christian Specht 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
