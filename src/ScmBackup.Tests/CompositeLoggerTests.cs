b6b6bcaea0430eb4d98b73ff5ac6c3fb4d19609f
Christian Specht <christi@nspecht.de>
2016-01-19 22:15:19 +0100

    test CompositeLogger

---
+++ src/ScmBackup.Tests/CompositeLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class CompositeLoggerTests
+    {
+        [Fact]
+        public void CallsAllUnderlyingLoggers()
+        {
+            var loggers = new List<FakeLogger> { new FakeLogger(), new FakeLogger() };
+            var sut = new CompositeLogger(loggers);
+
+            sut.Log(LogLevel.Info, "foo");
+
+            foreach(var mock in loggers)
+            {
+                Assert.True(mock.LoggedSomething);
+                Assert.Equal(LogLevel.Info, mock.LastLogLevel);
+                Assert.Equal("foo", mock.LastMessage);
+            }
+
+        }
+    }
+}
