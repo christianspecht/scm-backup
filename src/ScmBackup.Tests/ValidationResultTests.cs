0aeda4c40bb216d61b5bf7ed7674430075ef6541
Christian Specht <christi@nspecht.de>
2016-03-06 22:38:51 +0100

    new ValidationResult + tests

---
+++ src/ScmBackup.Tests/ValidationResultTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class ValidationResultTests
+    {
+        [Fact]
+        public void AddMessageAddsSingleMessage()
+        {
+            var sut = new ValidationResult();
+            sut.AddMessage(ErrorLevel.Info, "i");
+
+            Assert.Equal(1, sut.Messages.Count);
+        }
+
+        [Fact]
+        public void IsValidIsTrueWithNoMessages()
+        {
+            var sut = new ValidationResult();
+
+            Assert.False(sut.Messages.Any());
+            Assert.True(sut.IsValid);
+        }
+
+        [Fact]
+        public void IsValidIsTrueWithInfoAndWarnMessage()
+        {
+            var sut = new ValidationResult();
+            sut.AddMessage(ErrorLevel.Info, "i");
+            sut.AddMessage(ErrorLevel.Warn, "w");
+
+            Assert.Equal(2, sut.Messages.Count);
+            Assert.True(sut.IsValid);
+        }
+
+        [Fact]
+        public void IsValidIsFalseWithErrorMessage()
+        {
+            var sut = new ValidationResult();
+            sut.AddMessage(ErrorLevel.Error, "e");
+
+            Assert.Equal(1, sut.Messages.Count);
+            Assert.False(sut.IsValid);
+        }
+
+        [Fact]
+        public void IsValidIsFalseWithAllMessages()
+        {
+            var sut = new ValidationResult();
+            sut.AddMessage(ErrorLevel.Info, "i");
+            sut.AddMessage(ErrorLevel.Warn, "w");
+            sut.AddMessage(ErrorLevel.Error, "e");
+
+            Assert.Equal(3, sut.Messages.Count);
+            Assert.False(sut.IsValid);
+        }
+    }
+}
---
+++ src/ScmBackup/ValidationResult.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ScmBackup
+{
+    /// <summary>
+    /// return value for validators
+    /// </summary>
+    internal class ValidationResult
+    {
+        public ValidationResult()
+        {
+            this.Messages = new List<ValidationMessage>();
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return !this.Messages.Any(m => m.Error == ErrorLevel.Error);
+            }
+        }
+
+        public List<ValidationMessage> Messages { get; private set; }
+
+        public void AddMessage(ErrorLevel error, string message)
+        {
+            this.Messages.Add(new ValidationMessage(error, message));
+        }
+
+        internal class ValidationMessage
+        {
+            public ValidationMessage(ErrorLevel error, string message)
+            {
+                this.Error = error;
+                this.Message = message;
+            }
+
+            public ErrorLevel Error { get; private set; }
+            public string Message { get; private set; }
+        }
+    }
+}
