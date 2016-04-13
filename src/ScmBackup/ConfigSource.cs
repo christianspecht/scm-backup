ee3941b8b78be46f844b4ce73cad910ee7efc607
Christian Specht <christi@nspecht.de>
2016-04-13 18:21:40 +0200

    comment fields to avoid confusion

--- src/ScmBackup/ConfigSource.cs
+++ src/ScmBackup/ConfigSource.cs
@@ -1,12 +1,24 @@
 ﻿namespace ScmBackup
 {
     /// <summary>
-    /// subclass for Config
+    /// Configuration data to get the repositories of user X from hoster Y
+    /// (subclass for Config)
     /// </summary>
     internal class ConfigSource
     {
+        /// <summary>
+        /// name of the hoster
+        /// </summary>
         public string Hoster { get; set; }
+
+        /// <summary>
+        /// user type (e.g. user/team)
+        /// </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// user name
+        /// </summary>
         public string Name { get; set; }
     }
 }
