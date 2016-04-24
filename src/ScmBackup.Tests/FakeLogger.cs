ef0d538d59f65c51fadadd6e575500a50dcb02db
Christian Specht <christi@nspecht.de>
2016-04-24 23:30:06 +0200

    ignore debug logs in the tests

--- src/ScmBackup.Tests/FakeLogger.cs
+++ src/ScmBackup.Tests/FakeLogger.cs
@@ -10,6 +10,15 @@ namespace ScmBackup.Tests
         public string LastMessage { get; set; }
         public object[] LastArg { get; set; }
 
+        public bool IgnoreDebugLogs { get; set; }
+
+        public FakeLogger()
+        {
+            // default setting: ignore all debug logs, because they "get in the way" 
+            // when checking whether the last log was an error, for example
+            this.IgnoreDebugLogs = true;
+        }
+
         public void Log(ErrorLevel level, string message, params object[] arg)
         {
             this.Log(level, null, message, arg);
@@ -17,6 +26,11 @@ namespace ScmBackup.Tests
 
         public void Log(ErrorLevel level, Exception ex, string message, params object[] arg)
         {
+            if (level == ErrorLevel.Debug && this.IgnoreDebugLogs)
+            {
+                return;
+            }
+
             this.LoggedSomething = true;
             this.LastErrorLevel = level;
             this.LastException = ex;
