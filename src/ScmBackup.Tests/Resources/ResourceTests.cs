1cc6c2e5984f85b1efb478278307b97c9755e941
Christian Specht <christi@nspecht.de>
2016-05-16 19:46:25 +0200

    static helper class to get access to ResourceProvider

---
+++ src/ScmBackup.Tests/Resources/ResourceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace ScmBackup.Tests.Resources
+{
+    public class ResourceTests
+    {
+        [Fact]
+        public void ThrowsWhenInitializedWithoutProvider()
+        {
+            Resource.ResetProvider();
+            Assert.Throws<ArgumentNullException>(() => Resource.Initialize(null, new CultureInfo("en-US")));
+        }
+
+        [Fact]
+        public void GetStringThrowsWhenNotInitialized()
+        {
+            Resource.ResetProvider();
+            Assert.Throws<ArgumentNullException>(() => Resource.GetString("foo"));
+        }
+
+        [Fact]
+        public void InitializeCallsUnderlyingProvider()
+        {
+            var provider = new FakeResourceProvider();
+            var culture = new CultureInfo("en-US");
+
+            Resource.Initialize(provider, culture);
+
+            Assert.True(provider.InitializeWasCalled);
+            Assert.Equal(culture, provider.LastCulture);
+        }
+
+        [Fact]
+        public void GetStringReturnsStringWhenInitialized()
+        {
+            var provider = new FakeResourceProvider();
+            provider.StringToReturn = "foo";
+            var culture = new CultureInfo("en-US");
+            Resource.Initialize(provider, culture);
+
+            string result = Resource.GetString("bar");
+
+            Assert.True(provider.GetStringWasCalled);
+            Assert.Equal("bar", provider.LastKey);
+            Assert.Equal(provider.StringToReturn, result);
+        }
+    }
+}
---
+++ src/ScmBackup/Resource.cs
@@ -0,0 +1,40 @@
+﻿using ScmBackup.Resources;
+using System;
+using System.Globalization;
+
+namespace ScmBackup
+{
+    /// <summary>
+    /// Provides static access to localized string resources
+    /// </summary>
+    internal static class Resource
+    {
+        private static IResourceProvider internalProvider = null;
+        
+        public static void ResetProvider()
+        {
+            internalProvider = null;
+        }
+
+        public static void Initialize(IResourceProvider provider, CultureInfo culture)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider is null");
+            }
+
+            internalProvider = provider;
+            provider.Initialize(culture);
+        }
+
+        public static string GetString(string key)
+        {
+            if (internalProvider == null)
+            {
+                throw new ArgumentNullException("internal provider is null");
+            }
+
+            return internalProvider.GetString(key);
+        }
+    }
+}
