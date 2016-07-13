f24c3db2ebd87f211ebb664d3c5fb18941e98b62
Christian Specht <christi@nspecht.de>
2016-07-13 22:57:09 +0200

    add integration test for HosterValidator

---
+++ src/ScmBackup.Tests.Integration/HosterValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using ScmBackup.CompositionRoot;
+using ScmBackup.Tests.Hosters;
+using System.Linq;
+using Xunit;
+
+namespace ScmBackup.Tests.Integration
+{
+    public class HosterValidatorTests
+    {
+        [Fact]
+        public void ValidateCallsUnderlyingValidator()
+        {
+            var reader = new FakeConfigReader();
+            reader.SetDefaultFakeConfig();
+            var config = reader.ReadConfig();
+            var source = config.Sources.First();
+
+            var hoster = new FakeHoster();
+            var factory = new HosterFactory();
+            factory.Add(hoster);
+
+            var sut = new HosterValidator(factory);
+            sut.Validate(source);
+
+            Assert.True(hoster.FakeValidator.WasValidated);
+        }
+    }
+}
