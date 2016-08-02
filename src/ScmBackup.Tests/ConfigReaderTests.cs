d5b7d46d4179a4ea52c52f36dfc787ee5e619059
Christian Specht <christi@nspecht.de>
2016-08-02 22:59:57 +0200

    fix broken test (check for more specific exception to the test would fail for a FileNotFoundException)

--- src/ScmBackup.Tests/ConfigReaderTests.cs
+++ src/ScmBackup.Tests/ConfigReaderTests.cs
@@ -35,7 +35,7 @@ namespace ScmBackup.Tests
             var sut = new ConfigReader();
             sut.ConfigFileName = "brokensettings.json";
 
-            Assert.ThrowsAny<Exception>(() => sut.ReadConfig());
+            Assert.ThrowsAny<FormatException>(() => sut.ReadConfig());
         }
     }
 }
