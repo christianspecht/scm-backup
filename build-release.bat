a7d5a596a01ef3cb28c03de4cadc1f6596e1f07e
Christian Specht <christi@nspecht.de>
2016-09-01 20:02:28 +0200

    build script for release (run tests/publish)

---
+++ build-release.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo ###### UNIT TESTS ######
+dotnet test "%~dp0\src\ScmBackup.Tests" -c Release
+if errorlevel 1 goto end
+
+echo .
+echo ###### INTEGRATION TESTS ######
+dotnet test "%~dp0\src\ScmBackup.Tests.Integration" -c Release
+if errorlevel 1 goto end
+
+echo .
+echo ###### PUBLISH ######
+dotnet publish "%~dp0\src\ScmBackup" -c Release -o "%~dp0\release"
+if errorlevel 1 goto end
+
+:end
+pause
--- cleanup.bat
+++ cleanup.bat
@@ -1,3 +1,3 @@
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-
+rmdir /s /q "%~dp0\release"
