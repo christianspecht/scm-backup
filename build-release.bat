cccf523e23507a7237358c809fae47f607652997
Christian Specht <christi@nspecht.de>
2016-10-29 23:27:37 +0200

    delete "release" folder only when not on AppVeyor

--- build-release.bat
+++ build-release.bat
@@ -1,17 +1,25 @@
 @echo off
 
-rd /s /q release
+echo ###### INITIALIZE ######
+if [%APPVEYOR%] == [] (
+    rd /s /q release
+) else (
+    choco install 7zip.commandline -version 15.12
+)
 
 dotnet restore
+
 
 echo ###### UNIT TESTS ######
 dotnet test "%~dp0\src\ScmBackup.Tests" -c Release
 if errorlevel 1 goto end
 
+
 echo .
 echo ###### INTEGRATION TESTS ######
 dotnet test "%~dp0\src\ScmBackup.Tests.Integration" -c Release
 if errorlevel 1 goto end
+
 
 echo .
 echo ###### PUBLISH ######
@@ -21,14 +29,11 @@ if errorlevel 1 goto end
 
 echo .
 echo ###### ZIP ######
-if [%APPVEYOR%] == [True] (
-    choco install 7zip.commandline -version 15.12
-)
 call 7za a -r -tzip release\scm-backup.zip .\release\bin\*
 
 
 
 :end
-IF [%APPVEYOR%] == [] (
+if [%APPVEYOR%] == [] (
     pause
 )
