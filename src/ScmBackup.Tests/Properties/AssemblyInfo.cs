d53784b02f1e47950f544a90aa384c3ac39280c3
Christian Specht <christi@nspecht.de>
2016-11-02 17:51:03 +0100

    move version number to AssemblyInfo so it can be set by AppVeyor
    use "dev" version number for local builds because we'll let AppVeyor build official releases

--- src/ScmBackup.Tests/Properties/AssemblyInfo.cs
+++ src/ScmBackup.Tests/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-DEV")]
 [assembly: AssemblyTitle("ScmBackup.Tests")]
 [assembly: AssemblyDescription("Test library for SCM Backup")]
 [assembly: AssemblyConfiguration("")]
--- src/ScmBackup/Properties/AssemblyInfo.cs
+++ src/ScmBackup/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-DEV")]
 [assembly: AssemblyTitle("SCM Backup")]
 [assembly: AssemblyDescription("Backup repositories from cloud hosters to your local machine")]
 [assembly: AssemblyConfiguration("")]
