8ed3ff9ec8486030bcae8e5b2a6cbe1d776ba176
Christian Specht <christi@nspecht.de>
2016-11-04 16:09:55 +0100

    test Context class

---
+++ src/ScmBackup.Tests/ContextTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class ContextTests
+    {
+        [Fact]
+        public void DoesNotThrowExceptions()
+        {
+            // most of the Context class is .NET Framework functionality (which we don't want to test again),
+            // but at least we want to be noticed when anything throws an exception
+            var sut = new Context();
+
+            var version = sut.VersionNumber;
+            string versionString = sut.VersionNumberString;
+            string appTitle = sut.AppTitle;
+        }
+    }
+}
