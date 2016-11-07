4a5a3e0c6e64d73adb8218e0242f91f113a8fdfa
Christian Specht <christi@nspecht.de>
2016-11-08 00:10:23 +0100

    new helper class for testing

---
+++ src/ScmBackup.Tests/TestHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ScmBackup.Tests
+{
+    /// <summary>
+    /// helper methods for testing
+    /// </summary>
+    public static class TestHelper
+    {
+        /// <summary>
+        /// Returns the value of an environment variable. Throws an exception when the variable doesn't exist.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <returns></returns>
+        public static string EnvVar(string variableName)
+        {
+            string result = Environment.GetEnvironmentVariable(variableName);
+
+            if (String.IsNullOrWhiteSpace(result))
+            {
+                throw new ArgumentException(string.Format("Environment variable {0} not found", variableName));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/ScmBackup.Tests/TestHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class TestHelperTests
+    {
+        [Fact]
+        public void ReturnsExistingVariable()
+        {
+            Environment.SetEnvironmentVariable("ThisVariableExists", "foo");
+
+            Assert.Equal("foo", TestHelper.EnvVar("ThisVariableExists"));
+        }
+
+        [Fact]
+        public void ThrowsWhenRequestedVariableDoesNotExist()
+        {
+            Assert.Throws<ArgumentException>(() => TestHelper.EnvVar("ThisVariableDoesNotExist"));
+        }
+    }
+}
