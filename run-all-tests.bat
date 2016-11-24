349e7d897134355875779a668fef6f9739fb7e53
Christian Specht <christi@nspecht.de>
2016-11-24 17:09:46 +0100

    error message when environment variables batch is missing

--- run-all-tests.bat
+++ run-all-tests.bat
@@ -2,6 +2,9 @@
 
 if exist "%~dp0\environment-variables.bat" (
     call "%~dp0\environment-variables.bat"
+) else (
+    echo environment-variables.bat is missing. Some of the integration tests will fail!
+    pause
 )
 
 dotnet test "%~dp0\src\ScmBackup.Tests"
