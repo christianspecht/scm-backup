1791379d16d40d22b9df96c785c3bc7a61041302
Christian Specht <christi@nspecht.de>
2016-12-14 19:05:14 +0100

    new class to hold all config sources with their repository lists

---
+++ src/ScmBackup.Tests/ApiRepositoriesTests.cs
@@ -0,0 +1,43 @@
+﻿using ScmBackup.Hosters;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class ApiRepositoriesTests
+    {
+        [Fact]
+        public void AddAddsItem()
+        {
+            var source = new ConfigSource();
+            source.Name = "foo";
+
+            var list = new List<HosterRepository>();
+
+            var sut = new ApiRepositories();
+            sut.AddItem(source, list);
+
+            Assert.Equal(1, sut.Dic.Count);
+        }
+
+        [Fact]
+        public void GetReposForSourceReturnsList()
+        {
+            var source = new ConfigSource();
+            source.Name = "foo";
+
+            var list = new List<HosterRepository>();
+            list.Add(new HosterRepository("foo", "http://foo", ScmType.Git));
+            list.Add(new HosterRepository("bar", "http://bar", ScmType.Git));
+
+            var sut = new ApiRepositories();
+            sut.AddItem(source, list);
+
+            var result = sut.GetReposForSource(source);
+
+            Assert.Equal(2, result.Count);
+            Assert.Equal("foo", result.First().Name);
+        }
+    }
+}
---
+++ src/ScmBackup/ApiRepositories.cs
@@ -0,0 +1,25 @@
+﻿using ScmBackup.Hosters;
+using System.Collections.Generic;
+
+namespace ScmBackup
+{
+    internal class ApiRepositories
+    {
+        public Dictionary<ConfigSource, List<HosterRepository>> Dic { get; private set; }
+
+        public ApiRepositories()
+        {
+            this.Dic = new Dictionary<ConfigSource, List<HosterRepository>>();
+        }
+
+        public void AddItem(ConfigSource config, List<HosterRepository> repos)
+        {
+            this.Dic.Add(config, repos);
+        }
+
+        public List<HosterRepository> GetReposForSource(ConfigSource config)
+        {
+            return this.Dic[config];
+        }
+    }
+}
