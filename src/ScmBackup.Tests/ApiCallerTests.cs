ed26940dbcf01a418fa2f966a7bc1d20ee6bb20c
Christian Specht <christi@nspecht.de>
2016-12-18 22:17:41 +0100

    move test preparation to constructor

--- src/ScmBackup.Tests/ApiCallerTests.cs
+++ src/ScmBackup.Tests/ApiCallerTests.cs
@@ -8,37 +8,43 @@ namespace ScmBackup.Tests
 {
     public class ApiCallerTests
     {
-        [Fact]
-        public void ExecutesHosterApiCallerForEachConfigSource()
+        private ConfigSource source1;
+        private ConfigSource source2;
+        private Config config;
+
+        private List<HosterRepository> list1;
+        private List<HosterRepository> list2;
+        private FakeHosterApiCaller hac;
+
+        public ApiCallerTests()
         {
             var reader = new FakeConfigReader();
             reader.SetDefaultFakeConfig();
-            var source1 = reader.FakeConfig.Sources.First();
+            source1 = reader.FakeConfig.Sources.First();
 
-            var source2 = new ConfigSource();
+            source2 = new ConfigSource();
             source2.Title = "title2";
             source2.Hoster = "fake";
             reader.FakeConfig.Sources.Add(source2);
 
-            var config = reader.ReadConfig();
+            this.config = reader.ReadConfig();
 
-
-            var list1 = new List<HosterRepository>();
+            list1 = new List<HosterRepository>();
             list1.Add(new HosterRepository("foo1", "http://foo1", ScmType.Git));
 
-            var list2 = new List<HosterRepository>();
+            list2 = new List<HosterRepository>();
             list2.Add(new HosterRepository("foo2", "http://foo2", ScmType.Git));
 
-            var hac = new FakeHosterApiCaller();
+            hac = new FakeHosterApiCaller();
             hac.Lists.Add(source1, list1);
             hac.Lists.Add(source2, list2);
+        }
 
-
-
-            var sut = new ApiCaller(hac);            
+        [Fact]
+        public void ExecutesHosterApiCallerForEachConfigSource()
+        {
+            var sut = new ApiCaller(hac);
             var result = sut.CallApis(config);
-
-
 
             Assert.Equal(2, hac.PassedConfigSources.Count);
             Assert.Equal(2, result.Dic.Count());
