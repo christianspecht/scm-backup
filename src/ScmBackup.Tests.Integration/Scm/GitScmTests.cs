3e12e9a900b125e86305c6432e5a48ede328ba0c
Christian Specht <christi@nspecht.de>
2017-01-19 19:19:57 +0100

    make sure Git is installed

--- src/ScmBackup.Tests.Integration/Scm/GitScmTests.cs
+++ src/ScmBackup.Tests.Integration/Scm/GitScmTests.cs
@@ -18,5 +18,15 @@ namespace ScmBackup.Tests.Integration.Scm
             var sut = new GitScm();
             sut.IsOnThisComputer(this.config);
         }
+
+        [Fact]
+        public void IsOnThisComputerReturnsTrue()
+        {
+            // Some of the other integration tests will need to use Git -> make sure that the machine running the tests has Git installed
+            var sut = new GitScm();
+            var result = sut.IsOnThisComputer(this.config);
+
+            Assert.True(result);
+        }
     }
 }
