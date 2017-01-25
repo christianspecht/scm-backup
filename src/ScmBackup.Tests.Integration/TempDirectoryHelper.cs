34dffdc44f1cba36d547bd2bf369730d21488ac2
Christian Specht <christi@nspecht.de>
2017-01-25 23:37:30 +0100

    helper class (for tests) to create temp directories

---
+++ src/ScmBackup.Tests.Integration/TempDirectoryHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ScmBackup.Tests.Integration
+{
+    /// <summary>
+    /// helper class to create unique temp directories for integration tests
+    /// </summary>
+    public class TempDirectoryHelper
+    {
+        public static string CreateTempDirectory()
+        {
+            string tempDir = Path.GetTempPath();
+            string newDir = "scm-backup-temp-" + DateTime.UtcNow.ToString("yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+            string finalDir = Path.Combine(tempDir, newDir);
+
+            if (Directory.CreateDirectory(finalDir) != null)
+            {
+                return finalDir;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/ScmBackup.Tests.Integration/TempDirectoryHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Xunit;
+
+namespace ScmBackup.Tests.Integration
+{
+    public class TempDirectoryHelperTests
+    {
+        [Fact]
+        public void DirectoryIsCreated()
+        {
+            var result = TempDirectoryHelper.CreateTempDirectory();
+
+            Assert.False(string.IsNullOrWhiteSpace(result));
+            Assert.True(Directory.Exists(result));
+        }
+    }
+}
