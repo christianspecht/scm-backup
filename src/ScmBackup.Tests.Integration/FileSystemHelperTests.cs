660e7202fcd40d8308ae535a3f68f288307af48e
Christian Specht <christi@nspecht.de>
2017-07-12 18:02:22 +0200

    helper class for file system operations

---
+++ src/ScmBackup.Tests.Integration/FileSystemHelperTests.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System;
+using Xunit;
+
+namespace ScmBackup.Tests.Integration
+{
+    public class FileSystemHelperTests
+    {
+        [Fact]
+        public void DirectoryIsEmpty_ReturnsTrueWhenEmpty()
+        {
+            string dir = DirectoryHelper.CreateTempDirectory("fsh-1");
+
+            Assert.True(FileSystemHelper.DirectoryIsEmpty(dir));
+        }
+
+        [Fact]
+        public void DirectoryIsEmpty_ReturnsFalseWhenDirContainsFile()
+        {
+            string dir = DirectoryHelper.CreateTempDirectory("fsh-2");
+            File.WriteAllText(Path.Combine(dir, "foo.txt"), "foo");
+
+            Assert.False(FileSystemHelper.DirectoryIsEmpty(dir));
+        }
+
+        [Fact]
+        public void DirectoryIsEmpty_ReturnsFalseWhenDirContainsSubdir()
+        {
+            string dir = DirectoryHelper.CreateTempDirectory("fsh-3");
+            Directory.CreateDirectory(Path.Combine(dir, "subdir"));
+
+            Assert.False(FileSystemHelper.DirectoryIsEmpty(dir));
+        }
+
+        [Fact]
+        public void DirectoryIsEmpty_ThrowsWhenDirDoesntExist()
+        {
+            string dir = DirectoryHelper.CreateTempDirectory("fsh-4");
+            string subDir = Path.Combine(dir, "doesnt-exist");
+
+            Assert.Throws<DirectoryNotFoundException>(() => FileSystemHelper.DirectoryIsEmpty(subDir));
+        }
+
+        [Fact]
+        public void DirectoryIsEmpty_ThrowsWhenDirIsMissing()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileSystemHelper.DirectoryIsEmpty(null));
+            Assert.Throws<ArgumentException>(() => FileSystemHelper.DirectoryIsEmpty(string.Empty));
+        }
+    }
+}
---
+++ src/ScmBackup/FileSystemHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ScmBackup
+{
+    /// <summary>
+    /// helper class for file system operations
+    /// </summary>
+    public class FileSystemHelper
+    {
+        /// <summary>
+        /// Checks whether the given directory is empty
+        /// </summary>
+        public static bool DirectoryIsEmpty(string path)
+        {
+            if (Directory.GetFiles(path).Any() || Directory.GetDirectories(path).Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
