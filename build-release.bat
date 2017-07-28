109bacb8526ab15913a32429cbaa5985eec2afd5
Christian Specht <christi@nspecht.de>
2017-07-29 01:48:54 +0200

    copy log file even if the tests failed

--- build-release.bat
+++ build-release.bat
@@ -41,8 +41,6 @@ echo .
 echo ###### INTEGRATION TESTS ######
 dotnet test "%~dp0\src\ScmBackup.Tests.Integration\ScmBackup.Tests.Integration.csproj" -c Release
 if errorlevel 1 goto end
-md "%~dp0\release"
-copy "%~dp0\src\ScmBackup.Tests.Integration\bin\Release\netcoreapp1.1\*.log" "%~dp0\release\test.log" /y
 
 
 echo .
@@ -58,6 +56,8 @@ call 7z a -r "release\%RELEASE_FILENAME%.zip" .\release\bin\*
 
 
 :end
+copy "%~dp0\src\ScmBackup.Tests.Integration\bin\Release\netcoreapp1.1\*.log" "%~dp0\release\test.log" /y
+
 if [%APPVEYOR%] == [] (
     pause
 )
