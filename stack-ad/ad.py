47792d046d739ab80337f40f89a55f8e2daff062
Christian Specht <christi@nspecht.de>
2017-10-14 13:13:11 +0200

    save image in subdirectory

--- stack-ad/ad.py
+++ stack-ad/ad.py
@@ -1,7 +1,14 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+import os
+
+# create dir for images
+dir = 'img'
+if not os.path.exists(dir):
+    os.makedirs(dir)
+
 
 img = Image.new('RGB', (600,500), 'white')
 
-img.save('ad.png')
+img.save(dir + '/ad.png')
 
 
