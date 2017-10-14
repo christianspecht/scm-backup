822697825af2a168078a345b3b8ce7aed353751c
Christian Specht <christi@nspecht.de>
2017-10-14 13:37:11 +0200

    draw box

--- stack-ad/ad.py
+++ stack-ad/ad.py
@@ -7,7 +7,13 @@ if not os.path.exists(dir):
     os.makedirs(dir)
 
 
+# empty image
 img = Image.new('RGB', (600,500), 'white')
+
+# blue box
+dr = ImageDraw.Draw(img)
+dr.rectangle(((0, 250), (600, 350)), fill='#239FE6')
+
 
 img.save(dir + '/ad.png')
 
