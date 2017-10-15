50b1f1a9b6afb99b1f6ae5b6d48e81c1277189c0
Christian Specht <christi@nspecht.de>
2017-10-15 22:28:26 +0200

    "Contribute"

--- stack-ad/ad.py
+++ stack-ad/ad.py
@@ -1,10 +1,16 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 import os
 
+
 # create dir for images
 dir = 'img'
 if not os.path.exists(dir):
     os.makedirs(dir)
+
+
+# define fonts
+font_logo = ImageFont.truetype('calibrib.ttf', 64)
+font = ImageFont.truetype('calibri.ttf', 36)
 
 
 # empty image
@@ -18,12 +24,20 @@ img.paste(logo, (35,35), logo)
 
 
 # logo text
-font_logo = ImageFont.truetype('calibrib.ttf', 64)
 dr.text((250,100), 'SCM Backup', font=font_logo, fill='black')
 
 
 # blue box
 dr.rectangle(((0, 250), (600, 350)), fill='#239FE6')
+
+
+# "Contribute" text
+dr.text((100,435), 'Contribute on GitHub', font=font, fill='black')
+
+# GitHub logo (download from https://github.com/logos, put into this directory)
+ghlogo = Image.open('GitHub-Mark-64px.png')
+img.paste(ghlogo, (430,420), ghlogo)
+
 
 
 # 2px black border
