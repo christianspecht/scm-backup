1cfc9b587bf4a1d4b1b3254ecfeaf7849d7c70f6
Christian Specht <christi@nspecht.de>
2017-10-15 16:43:45 +0200

    logo text

--- stack-ad/ad.py
+++ stack-ad/ad.py
@@ -9,6 +9,7 @@ if not os.path.exists(dir):
 
 # empty image
 img = Image.new('RGBA', (600,500), 'white')
+dr = ImageDraw.Draw(img)
 
 
 # logo
@@ -16,8 +17,12 @@ logo = Image.open('../img/logo200x200.png').convert("RGBA")
 img.paste(logo, (35,35), logo)
 
 
+# logo text
+font_logo = ImageFont.truetype('calibrib.ttf', 64)
+dr.text((250,100), 'SCM Backup', font=font_logo, fill='black')
+
+
 # blue box
-dr = ImageDraw.Draw(img)
 dr.rectangle(((0, 250), (600, 350)), fill='#239FE6')
 
 
