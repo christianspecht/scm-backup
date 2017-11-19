7ab3c2b02a65c4aa169148f5c58bea3403bfc754
Christian Specht <christi@nspecht.de>
2017-11-19 17:18:00 +0100

    validate URLs

---
+++ src/ScmBackup.Tests/Http/UrlValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using ScmBackup.Http;
+using Xunit;
+
+namespace ScmBackup.Tests.Http
+{
+    public class UrlValidatorTests
+    {
+        [Theory]
+        [InlineData("http://scm-backup.org", true)]
+        [InlineData("https://github.com", true)]
+        [InlineData("foo", false)]
+        [InlineData("file:///c:/foo.txt", false)]
+        public void ValidatesUrls(string url, bool expectedResult)
+        {
+            var sut = new UrlValidator();
+            Assert.Equal(expectedResult, sut.UrlIsValid(url));
+        }
+        
+    }
+}
---
+++ src/ScmBackup/Http/UrlValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ScmBackup.Http
+{
+    internal class UrlValidator
+    {
+        public bool UrlIsValid(string url)
+        {
+            // https://stackoverflow.com/a/7581824/6884
+            // (Uri.UriSchemeHttp and Uri.UriSchemeHttps replaced by strings because apparently they don't exist in .NET Core)
+            Uri uriResult;
+            return Uri.TryCreate(url, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == "http" || uriResult.Scheme == "https");
+        }
+    }
+}
