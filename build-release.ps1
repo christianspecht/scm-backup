d9aadecc72a4f415f1ea294f271011f4f2531453
Christian Specht <christi@nspecht.de>
2017-12-29 14:36:45 +0100

    Automatically create GitHub releases by creating a tag
    All non-release CI builds are assigned an "0.0.0.CI-xyz" version number.
    
    Fixes #5, because the version numbers of all "official" releases are set
    manually and don't contain AppVeyor's build number.

---
+++ build-release.ps1
@@ -0,0 +1,67 @@
+﻿Write-Host '###### INITIALIZE ######'
+
+if ($env:APPVEYOR) {
+
+    # We are on AppVeyor
+    # - environment variables are set in the AppVeyor settings
+    # - version-number.ps1 was already called before AssemblyInfo patching
+
+}
+else {
+
+    if (Test-Path -Path 'release') {
+        Remove-Item -Recurse -Force 'release'
+    }
+
+    if (Test-Path -Path $PSScriptRoot\environment-variables.ps1) {
+        & $PSScriptRoot\environment-variables.ps1
+    }
+    else {
+        Write-Host 'environment-variables.ps1 is missing. Build will be canceled!'
+        exit
+    }
+
+    & $PSScriptRoot\version-number.ps1
+}
+
+$release_filename = 'scm-backup-' + $env:ScmBackupLongVersion
+
+
+dotnet restore
+
+
+''
+Write-Host '###### BUILD SOLUTION ######'
+dotnet build -c Release
+if ($LASTEXITCODE -eq 1) {
+    exit
+}
+
+
+''
+Write-Host '###### UNIT TESTS ######'
+dotnet test "$PSScriptRoot\src\ScmBackup.Tests\ScmBackup.Tests.csproj" -c Release
+if ($LASTEXITCODE -eq 1) {
+    exit
+}
+
+
+''
+Write-Host '###### INTEGRATION TESTS ######'
+dotnet test "$PSScriptRoot\src\ScmBackup.Tests.Integration\ScmBackup.Tests.Integration.csproj" -c Release
+if ($LASTEXITCODE -eq 1) {
+    exit
+}
+
+
+''
+Write-Host '###### PUBLISH ######'
+dotnet publish "$PSScriptRoot\src\ScmBackup" -c Release -o "$PSScriptRoot\release\bin"
+if ($LASTEXITCODE -eq 1) {
+    exit
+}
+
+
+''
+Write-Host '###### ZIP ######'
+7z a -r "release\$release_filename.zip" .\release\bin\*
---
+++ version-number.ps1
@@ -0,0 +1,34 @@
+﻿if ($env:APPVEYOR) {
+    # we are on AppVeyor
+
+    $commit = $env:APPVEYOR_REPO_COMMIT.Substring(0,7)
+
+    if ($env:APPVEYOR_REPO_TAG -eq $true) {
+        # we are building a tag -> we are doing a release -> use the tag as version number
+        $shortversion = $env:APPVEYOR_REPO_TAG_NAME
+        $longversion = $shortversion + '.' + $commit
+    }
+    else {
+        # regular CI build, no release
+        $shortversion = '0.0.0'
+        $longversion = '0.0.0.CI-' + $env:APPVEYOR_BUILD_NUMBER + '-' + $commit
+    }
+}
+else {
+
+    # local build
+    
+    Set-Location $PSScriptRoot
+    $commit = git rev-parse --short HEAD
+
+    $shortversion = '0.0.0'
+    $longversion = '0.0.0.DEV-' + $commit
+}
+
+$env:ScmBackupCommit=$commit
+$env:ScmBackupShortVersion=$shortversion
+$env:ScmBackupLongVersion=$longversion
+
+Write-Host 'Commit: ' $env:ScmBackupCommit
+Write-Host 'Short Version: ' $env:ScmBackupShortVersion
+Write-Host 'Long Version: ' $env:ScmBackupLongVersion
