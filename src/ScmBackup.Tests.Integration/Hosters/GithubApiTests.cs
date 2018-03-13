0be9e1a91b79b42ead2f2d6a33d89f9207507827
Christian Specht <christi@nspecht.de>
2018-03-13 23:05:36 +0100

    move GithubApiTests to abstract base class (reusable for future other APIs)

--- src/ScmBackup.Tests.Integration/Hosters/GithubApiTests.cs
+++ src/ScmBackup.Tests.Integration/Hosters/GithubApiTests.cs
@@ -1,217 +1,25 @@
-﻿using ScmBackup.Hosters;
-using ScmBackup.Hosters.Github;
-using ScmBackup.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Security.Authentication;
-using Xunit;
+﻿using ScmBackup.Hosters.Github;
 
 namespace ScmBackup.Tests.Integration.Hosters
 {
-    public class GithubApiTests
+    public class GithubApiTests : IHosterApiTests
     {
-        [Fact]
-        public void CallsGithubApi_UnauthenticatedUser()
+        internal override string EnvVarPrefix
         {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "user";
-            config.Name = TestHelper.EnvVar("GithubApiTests_Name");
-
-            var logger = new TestLogger("CallsGithubApi_UnauthenticatedUser");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            var repoList = sut.GetRepositoryList(config);
-
-            // HTTP status ok?
-            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
-
-            // at least one result?
-            Assert.NotNull(repoList);
-            Assert.True(repoList.Count > 0);
-
-            // specific repo exists?
-            string expectedName = TestHelper.BuildRepositoryName(config.Name, TestHelper.EnvVar("GithubApiTests_Repo"));
-            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
-            Assert.NotNull(repo);
-            Assert.True(ValidateUrls(repo));
+            get { return "GithubApiTests"; }
         }
 
-        [Fact]
-        public void CallsGithubApi_NonExistingUser_ThrowsException()
+        internal override string ConfigHoster
         {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "user";
-            config.Name = "scm-backup-testuser-does-not-exist";
-
-            var logger = new TestLogger("CallsGithubApi_NonExistingUser_ThrowsException");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            List<HosterRepository> repoList;
-            Assert.Throws<InvalidOperationException>(() => repoList = sut.GetRepositoryList(config));
+            get { return "github"; }
         }
 
-        [Fact]
-        public void CallsGithubApi_AuthenticatedUser_InvalidPasswordThrowsException()
+        public GithubApiTests()
         {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "user";
-            config.Name = TestHelper.EnvVar("GithubApiTests_Name");
-            config.AuthName = config.Name;
-            config.Password = "invalid-password";
-
-            var logger = new TestLogger("CallsGithubApi_AuthenticatedUser_InvalidPasswordThrowsException");
+            var logger = new TestLogger(this.EnvVarPrefix);
             var request = HttpLogHelper.GetRequest(logger);
 
-            var sut = new GithubApi(request, logger);
-
-            List<HosterRepository> repoList;
-            Assert.Throws<AuthenticationException>(() => repoList = sut.GetRepositoryList(config)); 
-        }
-
-        [Fact]
-        public void CallsGithubApi_AuthenticatedUser()
-        {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "user";
-            config.Name = TestHelper.EnvVar("GithubApiTests_Name");
-            config.AuthName = config.Name;
-            config.Password = TestHelper.EnvVar("GithubApiTests_PW");
-
-            var logger = new TestLogger("CallsGithubApi_AuthenticatedUser");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            var repoList = sut.GetRepositoryList(config);
-
-            // HTTP status ok?
-            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
-
-            // at least one result?
-            Assert.NotNull(repoList);
-            Assert.True(repoList.Count > 0);
-
-            // specific repo exists?
-            string expectedName = TestHelper.BuildRepositoryName(config.Name, TestHelper.EnvVar("GithubApiTests_Repo"));
-            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
-            Assert.NotNull(repo);
-            Assert.True(ValidateUrls(repo));
-        }
-
-        [Fact]
-        public void CallsGithubApi_Organization_Unauthenticated()
-        {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "org";
-            config.Name = TestHelper.EnvVar("GithubApiTests_OrgName");
-
-            var logger = new TestLogger("CallsGithubApi_Organization_Unauthenticated");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            var repoList = sut.GetRepositoryList(config);
-
-            // HTTP status ok?
-            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
-
-            // at least one result?
-            Assert.NotNull(repoList);
-            Assert.True(repoList.Count > 0);
-
-            // specific repo exists?
-            string expectedName = TestHelper.BuildRepositoryName(config.Name, TestHelper.EnvVar("GithubApiTests_Repo"));
-            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
-            Assert.NotNull(repo);
-            Assert.True(ValidateUrls(repo));
-        }
-
-        [Fact]
-        public void CallsGithubApi_NonExistingOrg_ThrowsException()
-        {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "org";
-            config.Name = "scm-backup-testorg-does-not-exist";
-
-            var logger = new TestLogger("CallsGithubApi_Organization_Unauthenticated");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            List<HosterRepository> repoList;
-            Assert.Throws<InvalidOperationException>(() => repoList = sut.GetRepositoryList(config));
-        }
-
-        [Fact]
-        public void CallsGithubApi_Organization_Authenticated()
-        {
-            var config = new ConfigSource();
-            config.Hoster = "github";
-            config.Type = "org";
-            config.Name = TestHelper.EnvVar("GithubApiTests_OrgName");
-            config.AuthName = TestHelper.EnvVar("GithubApiTests_Name");
-            config.Password = TestHelper.EnvVar("GithubApiTests_PW");
-            
-            var logger = new TestLogger("CallsGithubApi_Organization_Authenticated");
-            var request = HttpLogHelper.GetRequest(logger);
-
-            var sut = new GithubApi(request, logger);
-
-            var repoList = sut.GetRepositoryList(config);
-
-            // HTTP status ok?
-            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
-
-            // at least one result?
-            Assert.NotNull(repoList);
-            Assert.True(repoList.Count > 0);
-
-            // specific repo exists?
-            string expectedName = TestHelper.BuildRepositoryName(config.Name, TestHelper.EnvVar("GithubApiTests_Repo"));
-            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
-            Assert.NotNull(repo);
-            Assert.True(ValidateUrls(repo));
-        }
-
-        private bool ValidateUrls(HosterRepository repo)
-        {
-            bool result = true;
-
-            var validator = new UrlValidator();
-            if (!validator.UrlIsValid(repo.CloneUrl))
-            {
-                return false;
-            }
-
-            if (repo.HasWiki)
-            {
-                if (!validator.UrlIsValid(repo.WikiUrl))
-                {
-                    return false;
-                }
-            }
-
-            if (repo.HasIssues)
-            {
-                if (!validator.UrlIsValid(repo.IssueUrl))
-                {
-                    return false;
-                }
-            }
-
-            return result;
+            this.sut = new GithubApi(request, logger);
         }
     }
 }
---
+++ src/ScmBackup.Tests.Integration/Hosters/IHosterApiTests.cs
@@ -0,0 +1,196 @@
+﻿using ScmBackup.Hosters;
+using ScmBackup.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Xunit;
+
+namespace ScmBackup.Tests.Integration.Hosters
+{
+    public abstract class IHosterApiTests
+    {
+        // environment variables with this prefix (from environment-variables.ps1) are used
+        internal abstract string EnvVarPrefix { get; }
+
+        // "hoster" value for config sources
+        internal abstract string ConfigHoster { get; }
+
+        // this needs to be created in the child classes' constructor:
+        internal IHosterApi sut;
+
+        [Fact]
+        public void SutWasSetInChildClass()
+        {
+            Assert.NotNull(this.sut);
+        }
+
+        [Fact]
+        public void GetRepositoryList_UnauthenticatedUser_Executes()
+        {
+            var source = new ConfigSource();
+            source.Hoster = this.ConfigHoster;
+            source.Type = "user";
+            source.Name = TestHelper.EnvVar(this.EnvVarPrefix, "Name");
+
+            var repoList = this.sut.GetRepositoryList(source);
+
+            // HTTP status ok?
+            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
+
+            // at least one result?
+            Assert.NotNull(repoList);
+            Assert.True(repoList.Count > 0);
+
+            // specific repo exists?
+            string expectedName = TestHelper.BuildRepositoryName(source.Name, TestHelper.EnvVar(this.EnvVarPrefix, "Repo"));
+            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
+            Assert.NotNull(repo);
+            Assert.True(ValidateUrls(repo));
+        }
+
+
+        [Fact]
+        public void GetRepositoryList_NonExistingUser_ThrowsException()
+        {
+            var source = new ConfigSource();
+            source.Hoster = this.ConfigHoster;
+            source.Type = "user";
+            source.Name = "scm-backup-testuser-does-not-exist";
+
+            List<HosterRepository> repoList;
+            Assert.ThrowsAny<Exception>(() => repoList = sut.GetRepositoryList(source));
+        }
+
+        [Fact]
+        public void GetRepositoryList_AuthenticatedUser_InvalidPasswordThrowsException()
+        {
+            var source = new ConfigSource();
+            source.Hoster = this.ConfigHoster;
+            source.Type = "user";
+            source.Name = TestHelper.EnvVar(this.EnvVarPrefix, "Name");
+            source.AuthName = source.Name;
+            source.Password = "invalid-password";
+
+            List<HosterRepository> repoList;
+            Assert.ThrowsAny<Exception>(() => repoList = sut.GetRepositoryList(source));
+        }
+
+        [Fact]
+        public void GetRepositoryList_AuthenticatedUser_Executes()
+        {
+            var source = new ConfigSource();
+            source.Hoster = "github";
+            source.Type = "user";
+            source.Name = TestHelper.EnvVar(this.EnvVarPrefix, "Name");
+            source.AuthName = source.Name;
+            source.Password = TestHelper.EnvVar(this.EnvVarPrefix, "PW");
+
+            var repoList = sut.GetRepositoryList(source);
+
+            // HTTP status ok?
+            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
+
+            // at least one result?
+            Assert.NotNull(repoList);
+            Assert.True(repoList.Count > 0);
+
+            // specific repo exists?
+            string expectedName = TestHelper.BuildRepositoryName(source.Name, TestHelper.EnvVar(this.EnvVarPrefix, "Repo"));
+            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
+            Assert.NotNull(repo);
+            Assert.True(ValidateUrls(repo));
+        }
+
+        [Fact]
+        public void GetRepositoryList_UnauthenticatedOrganization_Executes()
+        {
+            var source = new ConfigSource();
+            source.Hoster = this.ConfigHoster;
+            source.Type = "org";
+            source.Name = TestHelper.EnvVar(this.EnvVarPrefix, "OrgName");
+
+            var repoList = sut.GetRepositoryList(source);
+
+            // HTTP status ok?
+            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
+
+            // at least one result?
+            Assert.NotNull(repoList);
+            Assert.True(repoList.Count > 0);
+
+            // specific repo exists?
+            string expectedName = TestHelper.BuildRepositoryName(source.Name, TestHelper.EnvVar(this.EnvVarPrefix, "Repo"));
+            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
+            Assert.NotNull(repo);
+            Assert.True(ValidateUrls(repo));
+        }
+
+        [Fact]
+        public void GetRepositoryList_NonExistingOrganization_ThrowsException()
+        {
+            var source = new ConfigSource();
+            source.Hoster = this.ConfigHoster;
+            source.Type = "org";
+            source.Name = "scm-backup-testorg-does-not-exist";
+
+            List<HosterRepository> repoList;
+            Assert.ThrowsAny<Exception>(() => repoList = sut.GetRepositoryList(source));
+        }
+
+        [Fact]
+        public void GetRepositoryList_AuthenticatedOrganization_Executes()
+        {
+            var source = new ConfigSource();
+            source.Hoster = "github";
+            source.Type = "org";
+            source.Name = TestHelper.EnvVar(this.EnvVarPrefix, "OrgName");
+            source.AuthName = TestHelper.EnvVar(this.EnvVarPrefix, "Name");
+            source.Password = TestHelper.EnvVar(this.EnvVarPrefix, "PW");
+
+            var repoList = sut.GetRepositoryList(source);
+
+            // HTTP status ok?
+            Assert.Equal(HttpStatusCode.OK, sut.LastResult.Status);
+
+            // at least one result?
+            Assert.NotNull(repoList);
+            Assert.True(repoList.Count > 0);
+
+            // specific repo exists?
+            string expectedName = TestHelper.BuildRepositoryName(source.Name, TestHelper.EnvVar(this.EnvVarPrefix, "Repo"));
+            var repo = repoList.Where(r => r.Name == expectedName).FirstOrDefault();
+            Assert.NotNull(repo);
+            Assert.True(ValidateUrls(repo));
+        }
+
+        private bool ValidateUrls(HosterRepository repo)
+        {
+            bool result = true;
+
+            var validator = new UrlValidator();
+            if (!validator.UrlIsValid(repo.CloneUrl))
+            {
+                return false;
+            }
+
+            if (repo.HasWiki)
+            {
+                if (!validator.UrlIsValid(repo.WikiUrl))
+                {
+                    return false;
+                }
+            }
+
+            if (repo.HasIssues)
+            {
+                if (!validator.UrlIsValid(repo.IssueUrl))
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
