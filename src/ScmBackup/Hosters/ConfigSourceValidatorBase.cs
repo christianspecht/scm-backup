ad9c75311884d8b0c258e8286a685b8f8aa7cb68
Christian Specht <christi@nspecht.de>
2018-04-04 18:05:20 +0200

    move config validator to base class

---
+++ src/ScmBackup/Hosters/ConfigSourceValidatorBase.cs
@@ -0,0 +1,58 @@
+﻿namespace ScmBackup.Hosters
+{
+    /// <summary>
+    /// base class for all config source validators
+    /// </summary>
+    internal abstract class ConfigSourceValidatorBase : IConfigSourceValidator
+    {
+        /// <summary>
+        /// name of the hoster (the "hoster" value from the config source)
+        /// </summary>
+        public abstract string HosterName { get; }
+
+        /// <summary>
+        /// basic validation rules which are always the same
+        /// </summary>
+        public ValidationResult Validate(ConfigSource source)
+        {
+            var result = new ValidationResult(source);
+
+            if (source.Hoster != this.HosterName)
+            {
+                result.AddMessage(ErrorLevel.Error, string.Format(Resource.WrongHoster, source.Hoster));
+            }
+
+            if (source.Type != "user" && source.Type != "org")
+            {
+                result.AddMessage(ErrorLevel.Error, string.Format(Resource.WrongType, source.Type));
+            }
+
+            if (string.IsNullOrWhiteSpace(source.Name))
+            {
+                result.AddMessage(ErrorLevel.Error, Resource.NameEmpty);
+            }
+
+            bool authNameEmpty = string.IsNullOrWhiteSpace(source.AuthName);
+            bool passwordEmpty = string.IsNullOrWhiteSpace(source.Password);
+
+            if (authNameEmpty != passwordEmpty)
+            {
+                result.AddMessage(ErrorLevel.Error, Resource.AuthNameOrPasswortEmpty);
+            }
+            else if (authNameEmpty && passwordEmpty)
+            {
+                result.AddMessage(ErrorLevel.Warn, Resource.AuthNameAndPasswortEmpty);
+            }
+
+            return this.ValidateSpecific(result, source);
+        }
+
+        /// <summary>
+        /// hoster-specific validation rules - this CAN be implemented in the child classes IF the given hoster has special rules
+        /// </summary>
+        public ValidationResult ValidateSpecific(ValidationResult result, ConfigSource source)
+        {
+            return result;
+        }
+    }
+}
--- src/ScmBackup/Hosters/Github/GithubConfigSourceValidator.cs
+++ src/ScmBackup/Hosters/Github/GithubConfigSourceValidator.cs
@@ -3,40 +3,11 @@
     /// <summary>
     /// validator for GitHub repositories
     /// </summary>
-    internal class GithubConfigSourceValidator : IConfigSourceValidator
+    internal class GithubConfigSourceValidator : ConfigSourceValidatorBase
     {
-        public ValidationResult Validate(ConfigSource source)
+        public override string HosterName
         {
-            var result = new ValidationResult(source);
-
-            if (source.Hoster != "github")
-            {
-                result.AddMessage(ErrorLevel.Error, string.Format(Resource.WrongHoster, source.Hoster));
-            }
-
-            if (source.Type != "user" && source.Type != "org")
-            {
-                result.AddMessage(ErrorLevel.Error, string.Format(Resource.WrongType, source.Type));
-            }
-
-            if (string.IsNullOrWhiteSpace(source.Name))
-            {
-                result.AddMessage(ErrorLevel.Error, Resource.NameEmpty);
-            }
-
-            bool authNameEmpty = string.IsNullOrWhiteSpace(source.AuthName);
-            bool passwordEmpty = string.IsNullOrWhiteSpace(source.Password);
-
-            if (authNameEmpty != passwordEmpty)
-            {
-                result.AddMessage(ErrorLevel.Error, Resource.AuthNameOrPasswortEmpty);
-            }
-            else if (authNameEmpty && passwordEmpty)
-            {
-                result.AddMessage(ErrorLevel.Warn, Resource.AuthNameAndPasswortEmpty);
-            }
-
-            return result;
+            get { return "github"; }
         }
     }
 }
