afbd7e1af7fcc93908250655187ceb03ab5ee0be
Christian Specht <christi@nspecht.de>
2018-04-04 17:50:05 +0200

    move config validator tests to base class

--- src/ScmBackup.Tests/Hosters/GithubConfigSourceValidatorTests.cs
+++ src/ScmBackup.Tests/Hosters/GithubConfigSourceValidatorTests.cs
@@ -1,14 +1,9 @@
-﻿using System.Linq;
-using ScmBackup.Hosters;
-using Xunit;
-using ScmBackup.Hosters.Github;
+﻿using ScmBackup.Hosters.Github;
 
 namespace ScmBackup.Tests.Hosters
 {
-    public class GithubConfigSourceValidatorTests
+    public class GithubConfigSourceValidatorTests : IConfigSourceValidatorTests
     {
-        private ConfigSource config;
-
         public GithubConfigSourceValidatorTests()
         {
             config = new ConfigSource();
@@ -17,99 +12,8 @@ namespace ScmBackup.Tests.Hosters
             config.Name = "foo";
             config.AuthName = "authname";
             config.Password = "pass";
-        }
 
-        [Fact]
-        public void ReturnsErrorWhenHosterNotGithub()
-        {
-            config.Hoster = "foo";
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.False(result.IsValid);
-            Assert.Equal(1, result.Messages.Count);
-            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData("foo")]
-        public void ReturnsErrorWhenTypeIsInvalid(string value)
-        {
-            config.Type = value;
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.False(result.IsValid);
-            Assert.Equal(1, result.Messages.Count);
-            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
-        }
-
-        [Theory]
-        [InlineData("user")]
-        [InlineData("org")]
-        public void ValidatesWhenTypeIsValid(string value)
-        {
-            config.Type = value;
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.True(result.IsValid);
-            Assert.False(result.Messages.Any());
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public void ReturnsErrorWhenNameIsEmpty(string value)
-        {
-            config.Name = value;
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.False(result.IsValid);
-            Assert.Equal(1, result.Messages.Count);
-            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
-        }
-
-        [Theory]
-        [InlineData("user", "")]
-        [InlineData("user", null)]
-        [InlineData("", "password")]
-        [InlineData("", "null")]
-        public void ReturnsErrorWhenAuthNameORPasswordAreSet(string user, string password)
-        {
-            config.AuthName = user;
-            config.Password = password;
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.False(result.IsValid);
-            Assert.Equal(1, result.Messages.Count);
-            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData(" ")]
-        public void ReturnsWarningWhenAuthNameAndPasswordAreEmpty(string value)
-        {
-            config.AuthName = value;
-            config.Password = value;
-
-            var sut = new GithubConfigSourceValidator();
-            var result = sut.Validate(config);
-
-            Assert.True(result.IsValid);
-            Assert.Equal(1, result.Messages.Count);
-            Assert.Equal(ErrorLevel.Warn, result.Messages[0].Error);
+            sut = new GithubConfigSourceValidator();
         }
     }
 }
---
+++ src/ScmBackup.Tests/Hosters/IConfigSourceValidatorTests.cs
@@ -0,0 +1,107 @@
+﻿using ScmBackup.Hosters;
+using System.Linq;
+using Xunit;
+
+namespace ScmBackup.Tests.Hosters
+{
+    public abstract class IConfigSourceValidatorTests
+    {
+        // these need to be created in the child classes' constructor
+        internal ConfigSource config;
+        internal IConfigSourceValidator sut;
+
+        [Fact]
+        public void PropertiesWereSetInChildClass()
+        {
+            Assert.NotNull(config);
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void ReturnsErrorWhenHosterIsWrong()
+        {
+            config.Hoster = "foo";
+            
+            var result = sut.Validate(config);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(1, result.Messages.Count);
+            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("foo")]
+        public void ReturnsErrorWhenTypeIsInvalid(string value)
+        {
+            config.Type = value;
+
+            var result = sut.Validate(config);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(1, result.Messages.Count);
+            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
+        }
+
+        [Theory]
+        [InlineData("user")]
+        [InlineData("org")]
+        public void ValidatesWhenTypeIsValid(string value)
+        {
+            config.Type = value;
+            
+            var result = sut.Validate(config);
+
+            Assert.True(result.IsValid);
+            Assert.False(result.Messages.Any());
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ReturnsErrorWhenNameIsEmpty(string value)
+        {
+            config.Name = value;
+            
+            var result = sut.Validate(config);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(1, result.Messages.Count);
+            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
+        }
+
+        [Theory]
+        [InlineData("user", "")]
+        [InlineData("user", null)]
+        [InlineData("", "password")]
+        [InlineData("", "null")]
+        public void ReturnsErrorWhenAuthNameORPasswordAreSet(string user, string password)
+        {
+            config.AuthName = user;
+            config.Password = password;
+            
+            var result = sut.Validate(config);
+
+            Assert.False(result.IsValid);
+            Assert.Equal(1, result.Messages.Count);
+            Assert.Equal(ErrorLevel.Error, result.Messages[0].Error);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ReturnsWarningWhenAuthNameAndPasswordAreEmpty(string value)
+        {
+            config.AuthName = value;
+            config.Password = value;
+            
+            var result = sut.Validate(config);
+
+            Assert.True(result.IsValid);
+            Assert.Equal(1, result.Messages.Count);
+            Assert.Equal(ErrorLevel.Warn, result.Messages[0].Error);
+        }
+    }
+}
