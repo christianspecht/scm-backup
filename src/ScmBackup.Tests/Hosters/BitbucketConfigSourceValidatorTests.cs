a8ad0a20958c5d53784ccdd078ecba97f55074ed
Christian Specht <christi@nspecht.de>
2018-04-19 23:19:36 +0200

    add tests for BitbucketConfigSourceValidator

---
+++ src/ScmBackup.Tests/Hosters/BitbucketConfigSourceValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using ScmBackup.Hosters.Bitbucket;
+
+namespace ScmBackup.Tests.Hosters
+{
+    public class BitbucketConfigSourceValidatorTests : IConfigSourceValidatorTests
+    {
+        public BitbucketConfigSourceValidatorTests()
+        {
+            config = new ConfigSource();
+            config.Hoster = "bitbucket";
+            config.Type = "user";
+            config.Name = "foo";
+            config.AuthName = "authname";
+            config.Password = "pass";
+
+            sut = new BitbucketConfigSourceValidator();
+        }
+    }
+}
