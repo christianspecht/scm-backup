105004b39b00b3ef08f0ee6ae1dd73f0e008e700
Christian Specht <christi@nspecht.de>
2018-04-30 19:31:04 +0200

    add tests for Bitbucket API

---
+++ src/ScmBackup.Tests.Integration/Hosters/BitbucketApiTests.cs
@@ -0,0 +1,28 @@
+﻿using ScmBackup.Hosters.Bitbucket;
+using ScmBackup.Http;
+
+namespace ScmBackup.Tests.Integration.Hosters
+{
+    public class BitbucketApiTests : IHosterApiTests
+    {
+        internal override string EnvVarPrefix
+        {
+            get { return "BitbucketApiTests"; }
+        }
+
+        internal override string ConfigHoster
+        {
+            get { return "bitbucket"; }
+        }
+
+        internal override int Pagination_MinNumberOfRepos
+        {
+            get { return 11; } // https://developer.atlassian.com/bitbucket/api/2/reference/meta/pagination
+        }
+
+        public BitbucketApiTests()
+        {
+            this.sut = new BitbucketApi(new HttpRequest());
+        }
+    }
+}
