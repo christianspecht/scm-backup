813569f7a0c191a7bc9c1763eae11f1884fc556d
Christian Specht <christi@nspecht.de>
2018-05-08 14:11:24 +0200

    helper class to build clone URLs

---
+++ src/ScmBackup.Tests/Hosters/CloneUrlBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace ScmBackup.Tests.Hosters
+{
+    /// <summary>
+    /// Helper class to build clone URLs for various hosters
+    /// Not part of IHoster and its subclasses by purpose, so we can use it in the tests without needing to create a complete IHoster instance.
+    /// </summary>
+    public static class CloneUrlBuilder
+    {
+        public static string GithubCloneUrl(string username, string reponame)
+        {
+            return string.Format("https://github.com/{0}/{1}", username, reponame);
+        }
+
+        public static string BitbucketCloneUrl(string username, string reponame)
+        {
+            return string.Format("https://bitbucket.org/{0}/{1}", username, reponame);
+        }
+    }
+}
---
+++ src/ScmBackup.Tests/Hosters/CloneUrlBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace ScmBackup.Tests.Hosters
+{
+    public class CloneUrlBuilderTests
+    {
+        [Fact]
+        public void BuildsGithubCloneUrl()
+        {
+            var result = CloneUrlBuilder.GithubCloneUrl("foo", "bar");
+
+            Assert.Equal("https://github.com/foo/bar", result);
+        }
+
+        [Fact]
+        public void BuildsBitbucketCloneUrl()
+        {
+            var result = CloneUrlBuilder.BitbucketCloneUrl("foo", "bar");
+
+            Assert.Equal("https://bitbucket.org/foo/bar", result);
+        }
+    }
+}
