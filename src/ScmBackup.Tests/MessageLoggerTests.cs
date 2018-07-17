72c0297a52d1dd30009b4c4887009ef865b68fd8
Christian Specht <christi@nspecht.de>
2018-07-17 23:32:58 +0200

    new logger which writes LogMessages

---
+++ src/ScmBackup.Tests/MessageLoggerTests.cs
@@ -0,0 +1,27 @@
+﻿using ScmBackup.Loggers;
+using System.Linq;
+using Xunit;
+
+namespace ScmBackup.Tests
+{
+    public class MessageLoggerTests
+    {
+        [Fact]
+        public void LogsMessage()
+        {
+            var msg = new LogMessages();
+            var sut = new MessageLogger(msg);
+
+            sut.Log(ErrorLevel.Info, "foo {0}", "bar");
+
+            var result = msg.GetMessages().ToList();
+            Assert.Equal(1, result.Count);
+
+            string text = result.First();
+
+            Assert.Contains("foo", text);
+            Assert.Contains("bar", text);
+            Assert.Contains("Info", text);
+        }
+    }
+}
---
+++ src/ScmBackup/Loggers/MessageLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace ScmBackup.Loggers
+{
+    internal class MessageLogger : ILogger
+    {
+        private ILogMessages messages;
+
+        public MessageLogger(ILogMessages messages)
+        {
+            this.messages = messages;
+        }
+
+        public void Log(ErrorLevel level, string message, params object[] arg)
+        {
+            this.Log(level, null, message, arg);
+        }
+
+        public void Log(ErrorLevel level, Exception ex, string message, params object[] arg)
+        {
+            var tmp = new StringBuilder();
+            tmp.Append(level.ToString("f")); // https://stackoverflow.com/a/32726578/6884
+
+            if (ex != null)
+            {
+                tmp.Append(" ");
+                tmp.Append(ex.Message);
+            }
+
+            tmp.Append(" ");
+            tmp.AppendFormat(message, arg);
+
+            this.messages.AddMessage(tmp.ToString());
+        }
+    }
+}
