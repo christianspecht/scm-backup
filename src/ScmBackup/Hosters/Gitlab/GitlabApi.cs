9b3ac136611159c056aa8f31e767d3b3164901c5
Christian Specht <christi@nspecht.de>
2018-11-16 23:40:08 +0100

    start GitLab implementation

---
+++ src/ScmBackup/Hosters/Gitlab/GitlabApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScmBackup.Hosters.Gitlab
+{
+    internal class GitlabApi : IHosterApi
+    {
+        public List<HosterRepository> GetRepositoryList(ConfigSource config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ScmBackup/Hosters/Gitlab/GitlabHoster.cs
@@ -0,0 +1,16 @@
+﻿namespace ScmBackup.Hosters.Gitlab
+{
+    internal class GitlabHoster : IHoster
+    {
+        public GitlabHoster(IConfigSourceValidator validator, IHosterApi api, IBackup backup)
+        {
+            this.Validator = validator;
+            this.Api = api;
+            this.Backup = backup;
+        }
+
+        public IConfigSourceValidator Validator { get; private set; }
+        public IHosterApi Api { get; private set; }
+        public IBackup Backup { get; private set; }
+    }
+}
