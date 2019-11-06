1c52e702ebb4d25c8f2f70af88ee803b3160d374
Christian Specht <christi@nspecht.de>
2019-11-06 17:17:11 +0100

    empty class for backup

---
+++ src/ScmBackup/Hosters/Gitlab/GitlabBackup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScmBackup.Scm;
+
+namespace ScmBackup.Hosters.Gitlab
+{
+    internal class GitlabBackup : BackupBase
+    {
+        public override void BackupRepo(string subdir, ScmCredentials credentials)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
