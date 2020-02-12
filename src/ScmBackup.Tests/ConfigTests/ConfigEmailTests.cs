da2511cc860bb97e1cbb9646a7a92ffe2ebfd9ad
Christian Specht <christi@nspecht.de>
2020-02-12 18:11:55 +0100

    fix error because of changed namespaces

--- src/ScmBackup.Tests/ConfigTests/ConfigEmailTests.cs
+++ src/ScmBackup.Tests/ConfigTests/ConfigEmailTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScmBackup.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- src/ScmBackup.Tests/LoggingHosterApiCallerTests.cs
+++ src/ScmBackup.Tests/LoggingHosterApiCallerTests.cs
@@ -1,4 +1,5 @@
-﻿using ScmBackup.Hosters;
+﻿using ScmBackup.Configuration;
+using ScmBackup.Hosters;
 using ScmBackup.Tests.Hosters;
 using System.Collections.Generic;
 using Xunit;
