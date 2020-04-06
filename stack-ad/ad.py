d4aaf932c76a272f74c6dd2b889f741637adcb9c
Christian Specht <christi@nspecht.de>
2020-04-06 21:52:44 +0200

    move stuff around / new text

--- stack-ad/ad.py
+++ stack-ad/ad.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
 # define fonts
 font_logo = ImageFont.truetype('calibrib.ttf', 64)
+font_cont = ImageFont.truetype('calibrib.ttf', 40)
 font = ImageFont.truetype('calibri.ttf', 30)
 
 
@@ -13,25 +14,29 @@ dr = ImageDraw.Draw(img)
 
 # logo
 logo = Image.open('../img/logo128x128.png').convert("RGBA")
-img.paste(logo, (35,25), logo)
+img.paste(logo, (35,35), logo)
 
 
 # logo text
-dr.text((220,65), 'SCM Backup', font=font_logo, fill='black')
+dr.text((220,75), 'SCM Backup', font=font_logo, fill='black')
 
 
 # blue box
-dr.rectangle(((0, 170), (600, 250)), fill='#239FE6')
-dr.text((75,180), 'Makes offline backups of your cloud', font=font, fill='white')
-dr.text((75,210), 'hosted source code repositories', font=font, fill='white')
+dr.rectangle(((0, 190), (600, 270)), fill='#239FE6')
+dr.text((75,200), 'Makes offline backups of your cloud', font=font, fill='white')
+dr.text((75,230), 'hosted source code repositories', font=font, fill='white')
 
 
 # "Contribute" text
-dr.text((100,435), 'Contribute on GitHub', font=font, fill='black')
+dr.text((75,300), 'Help us implement support', font=font, fill='black')
+dr.text((75,330), 'for more hosting sites!', font=font, fill='black')
+dr.text((75,360), '(in C#)', font=font, fill='black')
+dr.text((50,430), 'Contribute on GitHub', font=font_cont, fill='black')
+
 
 # GitHub logo (download from https://github.com/logos, put into this directory)
 ghlogo = Image.open('GitHub-Mark-64px.png')
-img.paste(ghlogo, (430,420), ghlogo)
+img.paste(ghlogo, (450,420), ghlogo)
 
 
 
