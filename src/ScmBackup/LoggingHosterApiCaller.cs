d5ccb7b8d3d449cbd1346fd0595c1c4d3336a828
Christian Specht <christi@nspecht.de>
2020-06-03 21:46:14 +0200

    remove Mercurial repo warning again

--- src/ScmBackup/LoggingHosterApiCaller.cs
+++ src/ScmBackup/LoggingHosterApiCaller.cs
@@ -18,20 +18,7 @@ namespace ScmBackup
         public List<HosterRepository> GetRepositoryList(ConfigSource source)
         {
             this.logger.Log(ErrorLevel.Info, Resource.ApiGettingRepos, source.Title, source.Hoster);
-            var repos = this.caller.GetRepositoryList(source);
-
-            // #40: Bitbucket will remove all Mercurial repos on Jun 01 2020 -> show warning when the list contains at least one
-            if (source.Hoster == "bitbucket")
-            {
-                var hgrepos = repos.Where(r => r.Scm == ScmType.Mercurial).Count();
-
-                if (hgrepos > 0)
-                {
-                    this.logger.Log(ErrorLevel.Warn,Resource.BitbucketMercurialWarning, hgrepos);
-                }
-            }
-
-            return repos;
+            return this.caller.GetRepositoryList(source);
         }
     }
 }
