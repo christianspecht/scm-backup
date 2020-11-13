17e19696ef8c2d56b5f8146c0f2b7b732223547f
Christian Specht <christi@nspecht.de>
2020-11-14 00:29:46 +0100

    new "options" config section (nested dictionary)

---
+++ src/ScmBackup.Tests/Configuration/ConfigTests.cs
@@ -0,0 +1,44 @@
+﻿using ScmBackup.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ScmBackup.Tests.Configuration
+{
+    public class ConfigTests
+    {
+        private Config sut;
+
+        public ConfigTests()
+        {
+            this.sut = new Config();
+
+            // options
+            sut.Options = new Dictionary<string, Dictionary<string, object>>();
+
+            var foo = new Dictionary<string, object>();
+            foo.Add("test_bool", true);
+            foo.Add("test_string", "test");
+            foo.Add("test_int", 42);
+            sut.Options.Add("foo", foo);
+        }
+
+        [Theory]
+        [InlineData(typeof(bool), "foo", "test_bool", true)]
+        [InlineData(typeof(string), "foo", "test_string", "test")]
+        [InlineData(typeof(int), "foo", "test_int", 42)]
+        [InlineData(typeof(string), "foo", "bar", null)]                // bar doesn't exist
+        [InlineData(typeof(bool), "bar", "test_bool", null)]            // bar doesn't exist
+        public void GetOptions_Check(Type type, string key1, string key2, object expectedResult)
+        {
+            var result = this.sut.GetOption(type, key1, key2);
+
+            Assert.Equal(expectedResult, result);
+            if (result != null)
+            {
+                Assert.IsType(type, result);
+            }
+        }
+    }
+}
--- src/ScmBackup/Configuration/Config.cs
+++ src/ScmBackup/Configuration/Config.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ScmBackup.Configuration
 {
@@ -15,12 +16,48 @@ namespace ScmBackup.Configuration
 
         public List<ConfigSource> Sources { get; set; }
 
+        /// <summary>
+        /// Two-level "generic" dictionary for various options
+        /// </summary>
+        public Dictionary<string, Dictionary<string, object>> Options { get; set; }
+
         public ConfigEmail Email { get; set; }
 
         public Config()
         {
             this.Sources = new List<ConfigSource>();
             this.Scms = new List<ConfigScm>();
+        }
+
+        /// <summary>
+        /// Gets a value from the config options
+        /// </summary>
+        /// <param name="type">The value must be of this type</param>
+        /// <param name="key1">first level key</param>
+        /// <param name="key2">second level key</param>
+        /// <returns></returns>
+        public object GetOption(Type type, string key1, string key2)
+        {
+            if (this.Options == null || !this.Options.ContainsKey(key1))
+            {
+                return null;
+            }
+
+            var optionsSub = this.Options[key1];
+            if (optionsSub == null || !optionsSub.ContainsKey(key2))
+            {
+                return null;
+            }
+
+            var val = optionsSub[key2];
+            try
+            {
+                return Convert.ChangeType(val, type);
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
