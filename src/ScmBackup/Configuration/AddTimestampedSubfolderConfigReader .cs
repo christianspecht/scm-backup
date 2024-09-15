b05bb44df1bf4c935fab97aca2734ed2554f9f0a
Christian Specht <christi@nspecht.de>
2024-09-15 23:53:39 +0200

    normalize tabs/spaces and line endings, add to .editorconfig

--- src/ScmBackup/Configuration/AddTimestampedSubfolderConfigReader .cs
+++ src/ScmBackup/Configuration/AddTimestampedSubfolderConfigReader .cs
@@ -11,8 +11,8 @@ namespace ScmBackup.Configuration
     internal class AddTimestampedSubfolderConfigReader : IConfigReader
     {
         private readonly IConfigReader configReader;
-		private readonly IFileSystemHelper fileHelper;
-		private Config config = null;
+        private readonly IFileSystemHelper fileHelper;
+        private Config config = null;
 
         public AddTimestampedSubfolderConfigReader(IConfigReader configReader, IFileSystemHelper fileHelper)
         {
@@ -29,14 +29,14 @@ namespace ScmBackup.Configuration
 
             var config = this.configReader.ReadConfig();
 
-			if (config.Options.Backup.AddTimestampedSubfolder) {
+            if (config.Options.Backup.AddTimestampedSubfolder) {
                 string localFolder = config.LocalFolder;
                 string timestampFormat = config.Options.Backup.TimestampFormat;
-				string timestamp = DateTime.Now.ToString(timestampFormat);
+                string timestamp = DateTime.Now.ToString(timestampFormat);
                 this.fileHelper.CreateSubDirectory(localFolder, timestamp);
                 config.LocalFolder = Path.Combine(localFolder, timestamp);
-			}
-			
+            }
+            
             this.config = config;
             return config;
         }
